656dd7a37889fe8b9d095378e96b8c9de60b0aeb
Immanuel Michael <immanuel2112@gmail.com>
2020-07-23 01:07:37 +0200

    EOD - 22-Jul-2020

--- src/adm/sp/__init__.py
+++ src/adm/sp/__init__.py
@@ -1,14 +1,15 @@
-import requests
-from office365.runtime.auth.authentication_context import AuthenticationContext
-from office365.sharepoint.client_context import ClientContext
 import glob
-import pandas as pd
 import os
 
+from office365.runtime.auth.authentication_context import AuthenticationContext
+from office365.sharepoint.client_context import ClientContext
 from office365.sharepoint.file import File
 from office365.sharepoint.file_creation_information import FileCreationInformation
+from openpyxl import load_workbook
+from tempfile import NamedTemporaryFile
 
 path = '//nlyehvdcs4vwa73/BOAShared/Transform/Report/PH_GLD_S4H-PRO/PRO001 Material Master/ttMARA/Post/*.xlsx'
+temp_path = '//nlyehvdcs4vwa73/BOAShared/Transform/Report/PH_GLD_S4H-PRO/PRO001 Material Master/ttMARA/Post'
 files=glob.glob(path)
 print(files)
 
@@ -31,21 +32,29 @@ ctx.execute_query()
 
 if len(result) > 0:
     print("Folder has been found: {0}".format(result[0].properties["Name"]))
+
+# ctx.web.get_folder_by_server_relative_url(folder_url+"\\"+folder_name).files.create_upload_session(source_path=base_path, chunk_size=1024)
+# ctx.execute_query()
+
 for file in files:
+    wb = load_workbook(file)
+    with NamedTemporaryFile(dir=temp_path) as tmp:
+        wb.save(tmp.name+'.xlsx')
+        tmp.seek(0)
+        stream = tmp.read()
+
     f=open(file, 'r')
     newFile = File(f)
     print(f)
     file_info = FileCreationInformation()
-    file_info.content = newFile
+    file_info.content = stream
     file_info.url = os.path.basename(folder_url+"\\"+folder_name+'\\'+f.name)
     file_info.overwrite = True
-    target_file = ctx.web.get_folder_by_server_relative_url(folder_url+"\\"+folder_name).files.add(file_info)
+    target_file = ctx.web.get_folder_by_server_relative_url(folder_url+"\\"+folder_name).files.add(wb)
     ctx.execute_query()
 
-    target_file.set_property("Title", f.name)
     target_file.set_property("Name", f.name)
     ctx.execute_query()
-    # ctx.web.get_folder_by_server_relative_url(folder_url).folders.add_child(f)
-    # ctx.execute_query()
     # f.readlines()
     f.close()
+    wb.close()
